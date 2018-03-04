c88359c03108fb1e7853258ecc50bd7edc57538e
Shaoor Munir <shaoormunir@outlook.com>
2018-03-05 01:15:37 +0500

    Audio processing

--- EmotionDetection/EmotionNeuralNetwork.py
+++ EmotionDetection/EmotionNeuralNetwork.py
@@ -1,7 +1,9 @@
 import pickle
 import random
+from collections import defaultdict
 
 import numpy as np
+import xlsxwriter
 from sklearn.neural_network import MLPRegressor
 
 class NeuralNetworkModelEmotion:
@@ -112,6 +114,154 @@ class NeuralNetworkModelEmotion:
             print("The prediction for conscientiousness of file {} is: {} ".format(randomfilename, conscientiousnessModelEmotion.predict(features)))
             print("The actual value is: {} ".format(videosData['conscientiousness'][randomfilename]))
 
+            NNModelDict = defaultdict(dict)
+
+            NNModelDict['openness'] = opennessModelEmotion
+            NNModelDict['agreeableness'] = agreeablenessModelEmotion
+            NNModelDict['extraversion'] = extraversionModelEmotion
+            NNModelDict['conscientiousness'] = conscientiousnessModelEmotion
+            NNModelDict['neuroticism'] = neuroticismModelEmotion
+
+            NNDataFile = open("../AnnotationFiles/emotionnn.pkl", "wb")
+            pickle.dump(NNModelDict, NNDataFile)
+
+    def test (self):
+        self.modelDict = pickle.load(open("../ModelStorage/emotionnn.pkl", "rb"))
+
+
+        TestEmotionDictPickle = open("testdict.pickle", "rb")
+
+        TestEmotionDict = pickle.load(TestEmotionDictPickle)
+
+        workbook = xlsxwriter.Workbook("emotionnndata.xlsx", {'nan_inf_to_errors': True})
+
+        worksheet = workbook.add_worksheet()
+
+        bold = workbook.add_format({'bold': True})
+
+        worksheet.write('A1', 'Openness (Expected)', bold)
+        worksheet.write('B1', 'Openness (Actual)', bold)
+        worksheet.write('C1', 'Openness (Error)', bold)
+
+        worksheet.write('D1', 'Extraversion (Expected)', bold)
+        worksheet.write('E1', 'Extraversion (Actual)', bold)
+        worksheet.write('F1', 'Extraversion (Error)', bold)
+
+        worksheet.write('G1', 'Neuroticism (Expected)', bold)
+        worksheet.write('H1', 'Neuroticism (Actual)', bold)
+        worksheet.write('I1', 'Neuroticism (Error)', bold)
+
+        worksheet.write('J1', 'Agreeableness (Expected)', bold)
+        worksheet.write('K1', 'Agreeableness (Actual)', bold)
+        worksheet.write('L1', 'Agreeableness (Error)', bold)
+
+        worksheet.write('M1', 'Conscientiousness (Expected)', bold)
+        worksheet.write('N1', 'Conscientiousness (Actual)', bold)
+        worksheet.write('O1', 'Conscientiousness (Error)', bold)
+
+        videosDataFile = open("../AnnotationFiles/annotation_test.pkl", "rb")
+        print('Loading data from pickle file.')
+
+
+        videosData = pickle.load(videosDataFile, encoding='latin1')
+
+        print('Getting names of all the video files.')
+        videoNames = list(videosData['extraversion'].keys())
+
+        videosFilePath = 'TestVideos/'
+
+        opennessMSE = []
+        extraversionMSE = []
+        neuroticismMSE = []
+        conscientiousnessMSE = []
+        agreeablenessMSE = []
+
+        for i, videoName in enumerate(videoNames):
+
+            openness = 0
+
+            extraversion = 0
+
+            neuroticism = 0
+
+            agreeableness = 0
+
+            conscientiousness = 0
+
+            print("Video number: {}".format(i + 1))
+
+            position = videoName.find(".mp4")
+            audioFileName = videoName[:position] + '.wav'
+
+            if audioFileName in TestEmotionDict:
+
+                feature = []
+
+                feature.append(TestEmotionDict[audioFileName]['neutral'])
+
+                feature.append(TestEmotionDict[audioFileName]['happy'])
+
+                feature.append(TestEmotionDict[audioFileName]['sad'])
+
+                feature.append(TestEmotionDict[audioFileName]['angry'])
+
+                feature.append(TestEmotionDict[audioFileName]['fear'])
+
+                features = np.array(feature)
+                features = features.reshape(1, -1)
+
+                openness = self.modelDict['openness'].predict(features)
+
+                extraversion = self.modelDict['extraversion'].predict(features)
+
+                neuroticism = self.modelDict['neuroticism'].predict(features)
+
+                agreeableness = self.modelDict['agreeableness'].predict(features)
+
+                conscientiousness = self.modelDict['conscientiousness'].predict(features)
+
+                worksheet.write(i + 1, 0, videosData['openness'][videoName])
+
+                worksheet.write(i + 1, 1, openness)
+                worksheet.write(i + 1, 2, np.square(videosData['openness'][videoName] - openness))
+                opennessMSE.append(np.square(videosData['openness'][videoName] - openness))
+
+                worksheet.write(i + 1, 3, videosData['extraversion'][videoName])
+
+                worksheet.write(i + 1, 4, extraversion)
+                worksheet.write(i + 1, 5, np.square(videosData['extraversion'][videoName] - extraversion))
+                extraversionMSE.append(np.square(videosData['extraversion'][videoName] - extraversion))
+
+                worksheet.write(i + 1, 6, videosData['neuroticism'][videoName])
+
+                worksheet.write(i + 1, 7, neuroticism)
+                worksheet.write(i + 1, 8, np.square(videosData['neuroticism'][videoName] - neuroticism))
+                neuroticismMSE.append(np.square(videosData['neuroticism'][videoName] - neuroticism))
+
+                worksheet.write(i + 1, 9, videosData['agreeableness'][videoName])
+
+                worksheet.write(i + 1, 10, agreeableness)
+                worksheet.write(i + 1, 11, np.square(videosData['agreeableness'][videoName] - agreeableness))
+                agreeablenessMSE.append(np.square(videosData['agreeableness'][videoName] - agreeableness))
+
+                worksheet.write(i + 1, 12, videosData['conscientiousness'][videoName])
+
+                worksheet.write(i + 1, 13, conscientiousness)
+                worksheet.write(i + 1, 14, np.square(videosData['conscientiousness'][videoName] - conscientiousness))
+                conscientiousnessMSE.append(np.square(videosData['conscientiousness'][videoName] - conscientiousness))
+
+        workbook.close()
+
+        ErrorDict = defaultdict(float)
+
+        ErrorDict['openness'] = np.sqrt(np.average(opennessMSE))
+        ErrorDict['agreeableness'] = np.sqrt(np.average(agreeablenessMSE))
+        ErrorDict['neuroticism'] = np.sqrt(np.average(neuroticismMSE))
+        ErrorDict['conscientiousness'] = np.sqrt(np.average(conscientiousnessMSE))
+        ErrorDict['extraversion'] = np.sqrt(np.average(extraversionMSE))
+
+        return ErrorDict
+
 NeuralNetwork = NeuralNetworkModelEmotion()
 
 params = {
@@ -122,4 +272,5 @@ params = {
     'alpha': 0.00001
 }
 
-NeuralNetwork.generate(params)
+NeuralNetwork.test()
+
